3ed61947390800c1603bb51b8f4daaa156ee8c8d
Ivan Sologub <ivan.sologub@gmail.com>
2018-06-21 01:44:58 +0300

    add multiply adTagUrl (array)

--- src/sdk-impl.js
+++ src/sdk-impl.js
@@ -190,8 +190,10 @@ SdkImpl.prototype.initAdObjects = function() {
  */
 SdkImpl.prototype.requestAds = function() {
   const adsRequest = new google.ima.AdsRequest();
-  if (this.controller.getSettings().adTagUrl) {
-    adsRequest.adTagUrl = this.controller.getSettings().adTagUrl;
+  const adTagUrl = this.controller.getSettings().adTagUrl instanceof Array ? this.controller.getSettings().adTagUrl.shift() : this.controller.getSettings().adTagUrl;
+
+  if (adTagUrl) {
+    adsRequest.adTagUrl = adTagUrl;
   } else {
     adsRequest.adsResponse = this.controller.getSettings().adsResponse;
   }
@@ -399,7 +401,15 @@ SdkImpl.prototype.onContentPauseRequested = function(adEvent) {
 SdkImpl.prototype.onContentResumeRequested = function(adEvent) {
   this.adsActive = false;
   this.adPlaying = false;
-  this.controller.onAdBreakEnd();
+  if(this.controller.getSettings().adTagUrl instanceof Array && this.controller.getSettings().adTagUrl.length) {
+    // Process multiply adtag (more than one preroll or postroll at the same time)
+    this.controller.playerWrapper.vjsPlayer.ads._inLinearAdMode = false;
+    this.controller.playerWrapper.vjsPlayer.removeClass('vjs-ad-playing');
+    this.controller.playerWrapper.vjsPlayer.addClass('vjs-has-started');
+    this.requestAds();
+  } else {
+    this.controller.onAdBreakEnd();
+  }
   // Hide controls in case of future non-linear ads. They'll be unhidden in
   // content_pause_requested.
 };
